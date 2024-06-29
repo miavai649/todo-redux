5fa5e4183f5eedf7af87bd95dfb67debeec88920
miavai649 <mahmudulhaquenoor@gmail.com>
2024-06-29 13:34:38 +0600

    layout created

--- src/components/Todo/TodoCard.tsx
+++ src/components/Todo/TodoCard.tsx
@@ -1,5 +1,16 @@
 const TodoCard = () => {
-  return <div>This is todo card</div>
+  return (
+    <div className='bg-white p-3 rounded-md flex justify-between items-center'>
+      <input type='checkbox' name='' id='' />
+      <p className='font-semibold'>Todo Title</p>
+      <p>Time</p>
+      <p>Description</p>
+      <div className='space-x-5'>
+        <button>del</button>
+        <button>edit</button>
+      </div>
+    </div>
+  )
 }
 
 export default TodoCard
--- src/components/Todo/TodoContainer.tsx
+++ src/components/Todo/TodoContainer.tsx
@@ -1,5 +1,15 @@
 const TodoContainer = () => {
-  return <div></div>
+  return (
+    <div>
+      <button>Add task</button>
+      <button>Filter</button>
+      <div className='bg-red-400 w-full space-y-3 rounded-xl p-5'>
+        <div className='bg-white rounded-md p-5 flex justify-center items-center text-2xl font-bold'>
+          <p>There is no task pending</p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default TodoContainer
